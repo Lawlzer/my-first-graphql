e71c7830f49f903dec5025dbf6da0f318d00f7f2
Kevin Derp <kevindaspam@gmail.com>
2022-10-29 20:38:12 -0500

    ⚰️Remove unnecessary code from index.ts (rootValue) Implement /playground endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,51 +2,11 @@ import 'reflect-metadata';
 
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
+import playground from 'graphql-playground-middleware-express';
 import path from 'path';
 import { buildSchema } from 'type-graphql';
 
 import { UsersResolver } from '~/users/users.resolvers';
-
-const users = [
-	{ id: 1, name: 'John Doe', email: 'johndoe@gmail.com' },
-	{ id: 2, name: 'Jane Doe', email: 'janedoe@gmail.com' },
-	{ id: 3, name: 'Mike Doe', email: 'mikedoe@gmail.com' },
-];
-
-type User = {
-	id: number;
-	name: string;
-	email: string;
-};
-
-type UserInput = Pick<User, 'email' | 'name'>;
-
-const getUser = (args: { id: number }): User | undefined => users.find((u) => u.id === args.id);
-
-const getUsers = (): User[] => users;
-
-const createUser = (args: { input: UserInput }): User => {
-	const user = {
-		id: users.length + 1,
-		...args.input,
-	};
-	users.push(user);
-	return user;
-};
-
-const updateUser = (args: { user: User }): User => {
-	const index = users.findIndex((u) => u.id === args.user.id);
-	const targetUser = users[index];
-	if (targetUser) users[index] = args.user;
-	return targetUser;
-};
-
-const root = {
-	getUser,
-	getUsers,
-	createUser,
-	updateUser,
-};
 
 async function init() {
 	const schema = await buildSchema({
@@ -59,10 +19,11 @@ async function init() {
 		'/graphql',
 		graphqlHTTP({
 			schema: schema,
-			rootValue: root,
-			graphiql: true,
+			// rootValue: root,
 		})
 	);
+
+	app.get('/playground', playground({ endpoint: '/graphql' }));
 
 	const PORT = 3000;
 
